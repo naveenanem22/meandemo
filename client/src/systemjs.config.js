ba3b1baeb88d44fc4a8d2db990dc7a345987eb92
naveen <naveenanem22@gmail.com>
2017-10-24 22:48:06 +1100

    fixed systemjs-angular-loader.js loader issue. Now .js extension is not required

--- client/src/systemjs.config.js
+++ client/src/systemjs.config.js
@@ -9,12 +9,8 @@
       'npm:': 'node_modules/'
     },
     // map tells the System loader where to look for things
-    map: {
-      // our app is within the app folder
-      'app': 'src/app/main.js',
-      //'main':'client/src/main.js',
-      
-      
+    map: {      
+      app: 'app',      
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
@@ -33,11 +29,11 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        
+        main: '../src/app/main',
         defaultExtension: 'js',
         meta: {
           './*.js': {
-            loader: 'systemjs-angular-loader.js'
+            loader: '/src/systemjs-angular-loader.js'
           }
         }
       },
